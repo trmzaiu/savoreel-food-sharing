a3c5cbb5eb652436d5a90f9a8dae7a0cb864d4a3
Jiang <95130977+ititiu21193@users.noreply.github.com>
2024-12-08 15:38:06 +0700

    Initial
    add font + color

--- app/src/main/java/com/example/savoreel/ui/theme/Color.kt
+++ app/src/main/java/com/example/savoreel/ui/theme/Color.kt
@@ -1,11 +1,16 @@
 package com.example.savoreel.ui.theme
 
 import androidx.compose.ui.graphics.Color
+val primaryButtonColor = Color(0xFFB22222)
+val linkColor = Color(0xFF444444)
+val lineColor = Color(0xFFAAA5A5)
+val homeColor =  Color(0XB23B3D3D)
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val backgroundLightColor = Color(0xFFFDFDFD)
+val secondaryLightColor = Color(0xFFF7F5F5)
+val fontLightColor = Color(0xFF444444)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val backgroundDarkColor = Color(0xFF252728)
+val secondaryDarkColor = Color(0XFF3B3D3D)
+val fontDarkColor = Color(0xFFFDFDFD)
+
